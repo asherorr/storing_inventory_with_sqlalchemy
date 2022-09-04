f7c7a5f92483ada2c488c9e3d828b330eabc0c96
asherorr <asher@mailfence.com>
2022-09-04 14:00:07 -0400

    This commit will modify my models.py file to abide by PEP 8 style guidelines.

--- models.py
+++ models.py
@@ -4,13 +4,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-
-
-
 engine = create_engine('sqlite:///inventory.db', echo=False)
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
+
 
 class Product(Base):
     __tablename__ = "products"
